f02d2e1a31334c71e3f6de50e6f6765a6f294cae
Sunicha <sunicha.f@ku.th>
2022-08-29 23:50:58 +0700

    Spring Thymeleaf- Signup and Login

---
+++ src/main/java/th/ac/ku/restaurant/controller/HomeController.java
@@ -0,0 +1,18 @@
+package th.ac.ku.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Sawaddee");
+        // ต้องคืนค่าเป็นชื่อไฟล์ html template โดยในเมธอดนี้ คืนค่าเป็น home.html
+        return "home";
+    }
+
+
+
+}
---
+++ src/main/java/th/ac/ku/restaurant/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package th.ac.ku.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import th.ac.ku.restaurant.model.User;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    // SELECT * FROM User WHERE username = ‘username in parameter’
+    User findByUsername(String username);
+
+}
